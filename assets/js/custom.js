b60d587dce4573e3d4f62d9a28d1d4e75474c4a1
Okeke Johnpaul <okekejohnpaul12@gmail.com>
2020-12-10 03:24:31 +0100

    kk

---
+++ assets/js/custom.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>E- learn</title>
+    <meta content="" name="description">
+    <meta name="theme-color" content="#241084">
+    <meta content="" name="keywords">
+    <!--icons link-->
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+<body>
+
+
+
+</body>
+<script type="text/javascript" src="assets/js/jquery.min.js"> </script>
+<script type="text/javascript" src="assets/js/bootstrap.min.js"> </script>
+<script type="text/javascript">
+    $(document).ready(function() {
+       
+       
+    });
+</script>
+</html>
